51dd022914d6d32c3ee83ad4607b564422c07b23
Parker P <exxcaelum@gmail.com>
2016-07-18 15:43:00 -0600

    rake tasks created, models and relationships built

--- db/migrate/20160718211619_create_invoice_items.rb
+++ db/migrate/20160718211619_create_invoice_items.rb
@@ -1,8 +1,6 @@
 class CreateInvoiceItems < ActiveRecord::Migration[5.0]
   def change
     create_table :invoice_items do |t|
-      t.references :item, foreign_key: true
-      t.references :invoice, foreign_key: true
       t.string :quantity
       t.integer :unit_price
 
