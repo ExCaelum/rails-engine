09dd5ea6b8e50cde91a04acc43965139132041f6
Colin Osborn <colin.osborn@gmail.com>
2016-07-18 20:53:37 -0600

    InvoiceSearchController

--- app/controllers/api/v1/invoice_search_controller.rb
+++ app/controllers/api/v1/invoice_search_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::InvoiceSearchController < ApplicationController
 
   def show
     respond_with Invoice.find(params[:id]) if params[:id]
-    respond_with Invoice.find_by(name: params[:name]) if params[:name]
-    respond_with Invoice.find(params[:id]) if params[:id]
-    respond_with Invoice.find(params[:id]) if params[:id]
+    respond_with Invoice.find_by(status: params[:status]) if params[:status]
+    respond_with Invoice.find_by(created_at: params[:created_at]) if params[:created_at]
+    respond_with Invoice.find_by(updated_at: params[:updated_at]) if params[:updated_at]
   end
 
 end
