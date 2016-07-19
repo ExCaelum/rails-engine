1f13c12e3fa388c54adb199f1a02ad0315827cc5
Colin Osborn <colin.osborn@gmail.com>
2016-07-19 15:28:47 -0600

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,35 +2,35 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      namespace :invoices do
-        get "find", to: "search#show", defaults: {format: :json}
-        get "find_all", to: "search#index", defaults: {format: :json}
-        get "random", to: "search#index", defaults: {format: :json}
+      namespace :invoices, defaults: {format: :json} do
+        get "find", to: "search#show"
+        get "find_all", to: "search#index"
+        get "random", to: "search#index"
       end
       namespace :items do
-        get "find", to: "search#show", defaults: {format: :json}
-        get "find_all", to: "search#index", defaults: {format: :json}
-        get "random", to: "search#index", defaults: {format: :json}
+        get "find", to: "search#show"
+        get "find_all", to: "search#index"
+        get "random", to: "search#index"
       end
       namespace :invoice_items do
-        get "find", to: "search#show", defaults: {format: :json}
-        get "find_all", to: "search#index", defaults: {format: :json}
-        get "random", to: "search#index", defaults: {format: :json}
+        get "find", to: "search#show"
+        get "find_all", to: "search#index"
+        get "random", to: "search#index"
       end
       namespace :merchants do
-        get "find", to: "search#show", defaults: {format: :json}
-        get "find_all", to: "search#index", defaults: {format: :json}
-        get "random", to: "random#show", defaults: {format: :json}
+        get "find", to: "search#show"
+        get "find_all", to: "search#index"
+        get "random", to: "random#show"
       end
       namespace :transactions do
-        get "find", to: "search#show", defaults: {format: :json}
-        get "find_all", to: "search#index", defaults: {format: :json}
-        get "random", to: "random#show", defaults: {format: :json}
+        get "find", to: "search#show"
+        get "find_all", to: "search#index"
+        get "random", to: "random#show"
       end
       namespace :customer do
-        get "find", to: "search#show", defaults: {format: :json}
-        get "find_all", to: "search#index", defaults: {format: :json}
-        get "random", to: "random#show", defaults: {format: :json}
+        get "find", to: "search#show"
+        get "find_all", to: "search#index"
+        get "random", to: "random#show"
       end
 
       resources :items, only: [:index, :show], defaults: {format: :json}
