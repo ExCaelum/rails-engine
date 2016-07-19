efd35f6592e7bbe5cda1cfb4e24fa159c0165b00
Parker P <exxcaelum@gmail.com>
2016-07-18 20:58:29 -0600

    merchant record points built out

---
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::Merchants::SearchController < ApplicationController
+  respond_to :json, :xml
+
+  def index
+    respond_with Merchant.where(merchant_params)
+  end
+
+  def show
+    respond_with Merchant.find_by(merchant_params)
+  end
+
+  private
+
+  def merchant_params
+    params.permit(:id, :name, :created_at, :updated_at)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get "find", to: "search#show", defaults: {format: :json}
+        get "find_all", to: "search#index", defaults: {format: :json}
+        get "random", to: "random#show", defaults: {format: :json}
+      end
+
       resources :invoices, only: [:index], defaults: {format: :json}
       resources :merchants, only: [:index, :show], defaults: {format: :json}
     end
