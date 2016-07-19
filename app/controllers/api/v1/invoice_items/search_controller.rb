1a8a952a472532ad8132ba026f03f0df1821fe86
Colin Osborn <colin.osborn@gmail.com>
2016-07-18 22:37:13 -0600

    invoice_item serach controller

--- app/controllers/api/v1/invoice_items/search_controller.rb
+++ app/controllers/api/v1/invoice_items/search_controller.rb
@@ -2,17 +2,17 @@ class Api::V1::SearchController < ApplicationController
   respond_to :json, :xml
 
   def index
-    respond_with Invoice.where(invoice_params)
+    respond_with InvoiceItems.where(invoice_items_params)
   end
 
   def show
-    respond_with Invoice.find_by(invoice_params)
+    respond_with InvoiceItems.find_by(invoice_items_params)
   end
 
   private
 
-  def invoice_params
-    params.permit(:id, :customer_id, :merchant_id, :status, :created_at, :updated_at)
+  def invoice_items_params
+    params.permit(:id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at)
   end
 
 end
