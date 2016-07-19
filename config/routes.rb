908013c34b5d00d664f95eac9063f3cd4f58943b
Colin Osborn <colin.osborn@gmail.com>
2016-07-19 15:37:59 -0600

    not sure

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   namespace :api do
-    namespace :v1 do
-      namespace :invoices, defaults: {format: :json} do
+    namespace :v1, defaults: {format: :json} do
+      namespace :invoices do
         get "find", to: "search#show"
         get "find_all", to: "search#index"
         get "random", to: "search#index"
