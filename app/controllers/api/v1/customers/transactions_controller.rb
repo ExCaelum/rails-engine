c3431ce1e26430ffd07e7fa9a1a80aaf380980c2
Colin Osborn <colin.osborn@gmail.com>
2016-07-20 11:38:57 -0600

    transactions relationship

--- app/controllers/api/v1/customers/transactions_controller.rb
+++ app/controllers/api/v1/customers/transactions_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::Customers::TransactionsController < ApplicationController
   respond_to :json
 
   def index
-    respond_with Customer.find(params[:id]).transaction
+    customer = Customer.find(params[:id])
+    respond_with customer.transactions
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,15 @@
 class Customer < ApplicationRecord
   has_many :invoices
   has_many :transactions, through: :invoices
+
+
+  def transactions
+    trans = []
+    transactions = Transaction.joins(:invoice)
+    self.invoices.each do |invoice|
+      trans << transactions.where(invoice_id: invoice.id)
+    end
+    trans
+  end
+
 end
