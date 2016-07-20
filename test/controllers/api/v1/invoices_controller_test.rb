d0feaf20fa2f741dd96793315ad8cc663e0fa4da
Colin Osborn <colin.osborn@gmail.com>
2016-07-19 18:53:16 -0600

    invoices controller test

--- test/controllers/api/v1/invoices_controller_test.rb
+++ test/controllers/api/v1/invoices_controller_test.rb
@@ -1,10 +1,23 @@
 require "test_helper"
 
-class Api::V1::InvoicesController < ActionDispatch::IntegrationTest
-  test "can retrieve a list of invoices for index" do
+class Api::V1::InvoicesControllerTest < ActionDispatch::IntegrationTest
+  test "can retrieve a list of invoices on index page" do
     get "/api/v1/invoices"
 
     assert_response :success
+    invoices = JSON.parse(response.body)
+    assert_equal 3, invoices.count
+  end
 
+  test " can retrieve a single invoice on show page" do
+    invoice = invoices(:one)
+    get "/api/v1/invoices/#{invoice.id}"
+
+    assert_response :success
+    parsed_invoice = JSON.parse(response.body)
+    assert_equal invoice.id, parsed_invoice["id"]
+    assert_equal invoice.customer_id, parsed_invoice["customer_id"]
+    assert_equal invoice.merchant_id, parsed_invoice["merchant_id"]
+    assert_equal invoice.status, parsed_invoice["status"]
   end
 end
