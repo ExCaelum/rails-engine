8b735b77f87423f132f2ccd55650745970f00f35
Colin Osborn <colin.osborn@gmail.com>
2016-07-20 18:53:55 -0600

    stay as long as you can

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,10 +5,14 @@ class Merchant < ApplicationRecord
   def self.total_revenue_by_date(date)
     total = joins(invoices: [:transactions, :invoice_items])
     .where(transactions: {result: "success"})
-    .where(invoices: {created_at: date})
+    .where(invoices: {created_at: "#{date}"})
     .sum("invoice_items.quantity * invoice_items.unit_price").to_d / 100
     revenue = total.truncate.to_s + '.' + sprintf('%02d', (total.frac * 100).truncate)
     {"total_revenue" => revenue}
+  end
+
+  def self.most_revenue_by_quantity()
+
   end
 
   private
