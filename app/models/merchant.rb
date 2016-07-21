72cab9a87a6752f7e63334a6cacbc52b78bef6e9
Colin Osborn <colin.osborn@gmail.com>
2016-07-20 18:59:40 -0600

    refactored method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,22 +3,20 @@ class Merchant < ApplicationRecord
   has_many :invoices
 
   def self.total_revenue_by_date(date)
-    total = joins(invoices: [:transactions, :invoice_items])
-    .where(transactions: {result: "success"})
-    .where(invoices: {created_at: "#{date}"})
+    total = successful_transactions.where(invoices: {created_at: "#{date}"})
     .sum("invoice_items.quantity * invoice_items.unit_price").to_d / 100
     revenue = total.truncate.to_s + '.' + sprintf('%02d', (total.frac * 100).truncate)
     {"total_revenue" => revenue}
   end
 
-  def self.most_revenue_by_quantity()
+  def self.most_revenue_by_quantity(revenue)
 
   end
 
   private
 
   def self.successful_transactions
-    joins.(invoices: [:transactions, :invoice_items])
+    joins(invoices: [:transactions, :invoice_items])
     .where(transactions: {result: "success"})
   end
 end
