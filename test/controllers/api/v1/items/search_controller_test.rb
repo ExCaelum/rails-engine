47e6f914e99a3a59c8c5a3225ff4fa4932eb0eff
Parker P <exxcaelum@gmail.com>
2016-07-21 00:31:54 -0600

    some tests

--- test/controllers/api/v1/items/search_controller_test.rb
+++ test/controllers/api/v1/items/search_controller_test.rb
@@ -7,19 +7,20 @@ class Api::V1::Items::SearchControllerTest < ActionDispatch::IntegrationTest
     get "/api/v1/items/find?name=#{item.name}"
     assert_response :success
     response_item = JSON.parse(response.body)
-    assert_equal item, response_item
+    assert_equal item.id, response_item["id"]
   end
   test "can search by a price" do
     item = items(:one)
     get "/api/v1/items/find?unit_price=#{item.unit_price}"
     assert_response :success
     response_item = JSON.parse(response.body)
-    assert_equal item, response_item
+    assert_equal item.id, response_item["id"]
   end
   test "can get a random item" do
     get "/api/v1/items/random"
     assert_response :success
-    response_item = JSON.parse(response.body)
+    response_stuff = JSON.parse(response.body)
+    response_item = Item.find(response_stuff.first["id"])
     assert_equal Item, response_item.class
   end
 
